fcb166fe35fe92a31e90735b67a9713f8bfdee0d
Alizadekh <aykhanalizadekh@gmail.com>
2023-06-30 15:31:05 +0400

    FirstPage Completed

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function switchPhoto() {
+    document.getElementById("sec-1").style.backgroundImage = "url('https://preview.colorlib.com/theme/gamewarrior/img/slider-2.jpg')";
+    document.getElementById("sec-1").style.transition = "background-color 0.5s"
+}
+
+function revertPhoto() {
+    document.getElementById("sec-1").style.backgroundImage = "url('https://preview.colorlib.com/theme/gamewarrior/img/slider-1.jpg')";
+    document.getElementById("sec-1").style.transition = "background-color 0.5s"
+} 
+
